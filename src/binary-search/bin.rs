61c81775e780414d375890a14c435508ddeec35f
Fabien Bellanger <fabien.bellanger@apitic.com>
2023-06-09 08:56:21 +0200

    Improve code

--- src/binary-search/bin.rs
+++ src/binary-search/bin.rs
@@ -1,22 +1,18 @@
-fn binary_search<T: Ord + Copy>(list: &[T], item: T) -> Option<usize> {
-    if list.is_empty() {
-        return None;
-    }
+use std::cmp::Ordering;
 
-    let mut low = 0;
-    let mut hight = list.len() - 1;
+fn binary_search<T: PartialEq + Ord>(list: &[T], item: &T) -> Option<usize> {
+    if !list.is_empty() {
+        let mut low = 0;
+        let mut hight = list.len() - 1;
 
-    while low <= hight {
-        let middle = (low + hight) / 2;
-        let guess = list[middle];
+        while low <= hight {
+            let middle = (low + hight) / 2;
 
-        if guess == item {
-            return Some(middle);
-        }
-        else if guess > item {
-            hight = middle.saturating_sub(1);
-        } else {
-            low = middle.saturating_add(1);
+            match list[middle].cmp(item) {
+                Ordering::Equal => return Some(middle),
+                Ordering::Greater => hight = middle.checked_sub(1)?,
+                Ordering::Less => low = middle.checked_add(1)?,
+            }
         }
     }
 
@@ -24,20 +20,20 @@ fn binary_search<T: Ord + Copy>(list: &[T], item: T) -> Option<usize> {
 }
 
 fn main() {
-    let list = vec![12,56,102,568,700];
-    assert_eq!(binary_search(&list, 102), Some(2));
-    assert_eq!(binary_search(&list, 12), Some(0));
-    assert_eq!(binary_search(&list, 700), Some(4));
-    assert_eq!(binary_search(&list, 103), None);
+    let list = vec![12, 56, 102, 568, 700];
+    assert_eq!(binary_search(&list, &102), Some(2));
+    assert_eq!(binary_search(&list, &12), Some(0));
+    assert_eq!(binary_search(&list, &700), Some(4));
+    assert_eq!(binary_search(&list, &103), None);
 
-    let mut list = vec![700,568,102,56,12];
+    let mut list = vec![700, 568, 102, 56, 12];
     list.sort_unstable();
-    assert_eq!(binary_search(&list, 102), Some(2));
-    assert_eq!(binary_search(&list, 12), Some(0));
-    assert_eq!(binary_search(&list, 700), Some(4));
-    assert_eq!(binary_search(&list, 103), None);
+    assert_eq!(binary_search(&list, &102), Some(2));
+    assert_eq!(binary_search(&list, &12), Some(0));
+    assert_eq!(binary_search(&list, &700), Some(4));
+    assert_eq!(binary_search(&list, &103), None);
 
-    let list =  vec!["a", "b", "c", "d"];
-    assert_eq!(binary_search(&list, "a"), Some(0));
-    assert_eq!(binary_search(&list, "e"), None);
+    let list = vec!["a", "b", "c", "d"];
+    assert_eq!(binary_search(&list, &"a"), Some(0));
+    assert_eq!(binary_search(&list, &"e"), None);
 }

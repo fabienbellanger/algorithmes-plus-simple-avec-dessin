77c60e45bb7d0ecb260a6ea9fccc20173a6a81f6
Fabien Bellanger <fabien.bellanger@apitic.com>
2023-06-13 16:47:30 +0200

    More functional approach for selection sort

--- src/selection-sort/bin.rs
+++ src/selection-sort/bin.rs
@@ -1,30 +1,53 @@
-fn selection_sort<T: PartialOrd + Clone>(mut list: Vec<T>) -> Vec<T> {
+fn selection_sort<T: PartialOrd + Clone>(mut list: Vec<T>) -> Result<Vec<T>, ()> {
     let mut new_list = vec![];
 
     for _ in 0..list.len() {
-        let mut idx = 0;
-        let mut smallest = list[0].clone();
+        let mut min_idx = 0;
+        let mut smallest = list.get(0).ok_or(())?.clone();
 
         for (i, _) in list.iter().enumerate().skip(1) {
             if list[i] < smallest {
-                smallest = list[i].clone();
-                idx = i;
+                smallest = list.get(i).ok_or(())?.clone();
+                min_idx = i;
             }
         }
 
         new_list.push(smallest);
-        list.remove(idx);
+        list.remove(min_idx);
     }
 
-    new_list
+    Ok(new_list)
+}
+
+fn selection_sort_2<T: Ord>(list: &mut [T]) {
+    for i in 0..list.len() {
+        let min_idx = list[i..]
+            .iter()
+            .enumerate()
+            .min_by_key(|&(_, v)| v)
+            .map(|(i, _)| i)
+            .unwrap_or(0);
+
+        list.swap(i, min_idx + i);
+    }
 }
 
 fn main() {
     let list = vec![42, 2, 96, 74, 78, 78, 1, 9, 18];
     let expected = vec![1, 2, 9, 18, 42, 74, 78, 78, 96];
-    assert_eq!(selection_sort(list), expected);
+    assert_eq!(selection_sort(list).unwrap(), expected);
 
     let list = vec!["dog", "cat", "John", "car", "zorro", "at", "lime"];
     let expected = vec!["John", "at", "car", "cat", "dog", "lime", "zorro"];
-    assert_eq!(selection_sort(list), expected);
+    assert_eq!(selection_sort(list).unwrap(), expected);
+
+    let mut list = vec![42, 2, 96, 74, 78, 78, 1, 9, 18];
+    let expected = vec![1, 2, 9, 18, 42, 74, 78, 78, 96];
+    selection_sort_2(&mut list);
+    assert_eq!(list, expected);
+
+    let mut list = vec!["dog", "cat", "John", "car", "zorro", "at", "lime"];
+    let expected = vec!["John", "at", "car", "cat", "dog", "lime", "zorro"];
+    selection_sort_2(&mut list);
+    assert_eq!(list, expected);
 }

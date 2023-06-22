55a2d1a80dc2fa049c67ad2c67cc566ce3c80ba3
Fabien Bellanger <fabien.bellanger@apitic.com>
2023-06-22 18:38:02 +0200

    WIP

--- src/bfs/bin.rs
+++ src/bfs/bin.rs
@@ -1,7 +1,7 @@
 /*
 Example:
 
- ParcoursLargeur(Graphe G, Sommet s):
+ParcoursLargeur(Graphe G, Sommet s):
     f = CreerFile();
     f.enfiler(s);
     marquer(s);
@@ -31,6 +31,51 @@ def search(name):
     return False
 */
 
+use std::{
+    collections::{HashMap, HashSet},
+    fmt::Debug,
+    hash::Hash,
+};
+
+fn _person_to_search(name: &str) -> bool {
+    name.len() == 5
+}
+
+// TODO: Search function as parameter
+fn search<T: Hash + Eq + Clone + Debug>(graph: &HashMap<T, Vec<T>>, node: T) -> Option<T> {
+    let mut search_queue: Vec<T> = graph.get(&node).unwrap_or(&vec![]).clone();
+    let mut searched: HashSet<T> = HashSet::new();
+    searched.insert(node.clone());
+
+    println!("==> {node:?}");
+
+    while !search_queue.is_empty() {
+        println!("          {:?}", &search_queue);
+        println!("          {:?}", &searched);
+        let s = search_queue.pop().unwrap();
+
+        println!("==> {s:?}");
+
+        let next_nodes = graph.get(&s).unwrap_or(&vec![]).clone();
+        for n in next_nodes {
+            if searched.get(&n).is_none() {
+                search_queue.push(n.clone());
+                searched.insert(n);
+            }
+        }
+    }
+
+    None
+}
+
 fn main() {
     println!("Breadth-first search");
+    let mut graph = HashMap::new();
+    graph.insert("me", vec!["alice", "bob"]);
+    graph.insert("alice", vec!["john"]);
+    graph.insert("john", vec!["luke"]);
+    graph.insert("bob", vec!["yoyo"]);
+    graph.insert("yoyo", vec!["alice"]); // <- bug ?
+    search(&graph, "me");
+    // assert_eq!(search(HashMap::new(), "you"), None);
 }
